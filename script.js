d4608cd3b74e11973d3693744679e5142ae399a9
Alex Shitov <Shitov.aleksei01@Gmail.com>
2022-08-29 13:28:24 +0700

    Вынос js-кода в отдельный файл

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Всем привет!!!");
+console.error("Ошибка...");
+console.warn("Сообщение предупреждение");
+
+function notReadyAlert() {
+    alert( 'Part is not ready yet! \n Раздел не готов!' );
+    return false;
+}
+
+function search() {
+    let name = document.getElementById('search').value;
+    let productNumber = null;
+    if (name == 'НСК') {
+        productNumber = 0;
+    } 
+    else if (name == 'Президент') {
+        productNumber = 1;
+    } 
+    else if (name == 'Судья') {
+        productNumber = 2;
+    }
+    else {
+        alert("Товар не найден!!!");
+    }
+    let cards = document.getElementsByClassName('card');
+    let card = cards[productNumber];
+    card.style.border = '1px dashed red';
+    card.style.backgroundColor = 'yellow';
+    
+    setTimeout(function() {
+    card.style.border = 'none';
+    card.style.backgroundColor = '';
+}, 2000);
+}
+
+function loaded() {
+    let searchbox = document.getElementById('search');
+    searchbox.addEventListener('keydown', function (key){
+        if (key.key == 'Enter')
+        search();
+    });
+}
